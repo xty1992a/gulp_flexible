1b598694a8d18ea5031f80498362c357a562a010
xty <redbuck@126.com>
2017-08-07 23:46:28 +0800

    add: 添加html编译任务

--- gulpfile.js
+++ gulpfile.js
@@ -8,9 +8,10 @@ gulp.task('testLess', function () {
       .pipe(less()) //该任务调用的模块
       .pipe(gulp.dest('src/css')); //将会在src/css下生成index.css
 });
+gulp.task('html', function () {
+  gulp.src('./src/page/*.html')
+      .pipe(gulp.dest('./dist/page'))
+})
 
-gulp.task('default', ['testLess', 'elseTask']); //定义默认任务
+gulp.task('default', ['testLess', 'html']); //定义默认任务
 
-//gulp.task(name[, deps], fn) 定义任务  name：任务名称 deps：依赖任务名称 fn：回调函数
-//gulp.src(globs[, options]) 执行任务处理的文件  globs：处理的文件路径(字符串或者字符串数组)
-//gulp.dest(path[, options]) 处理完后文件生成路径
