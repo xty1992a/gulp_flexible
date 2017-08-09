9bfb67cbb8cb4fce0e1cf989b7a536d722fcea45
xty <redbuck@126.com>
2017-08-10 00:23:38 +0800

    add:引入svg图标

--- dist/js/index.js
+++ dist/js/index.js
@@ -40,17 +40,17 @@ $(function () {
   var list = $('.list')
   var menu = $('.menu')
   console.log('document ready', list)
-  list.on('click', 'li', function () {
+  list.on('tap', 'li', function () {
     $(this).slideUp(200)
     console.log(this)
   })
-  list.on('click', function () {
+  list.on('tap', function () {
     console.log('click')
   })
-  menu.on('click', 'li', function () {
+  menu.on('tap', 'li', function () {
     var index = $(this).index()
     console.log('tap')
-
     $(list.children()[index]).slideToggle(200)
   })
 })
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,65 +1,85 @@
 var gulp = require('gulp');
-var postcss = require('gulp-postcss');
-var px2rem = require('postcss-px2rem');
-var connect = require('gulp-connect');
-var concat = require('gulp-concat')
-var uglify = require('gulp-uglify')
+var px2rem = require('postcss-px2rem')
+var $ = require('gulp-load-plugins')();
 
 var path = {
   input: {
     js: './src/js/',
-    commonjs: './src/common/js/'
+    commonjs: './src/common/js/',
+    commonSvg: './src/common/svg/*.svg'
   },
   output: {
     js: './dist/js'
   }
 }
 
+gulp.task('js', function () {
+  return gulp.src([
+    path.input.commonjs + 'extend.js',
+    path.input.js + '*.js'
+  ])
+      .pipe($.concat('index.js'))
+      // .pipe($.uglify())
+      .pipe(gulp.dest(path.output.js))
+      .pipe($.connect.reload());
+});
+
+gulp.task('svg', function () {
+  return gulp.src(path.input.commonSvg)
+      .pipe($.svgSprite({
+        mode: {
+          symbol: {
+            prefix: `.svg-`,
+            dimensions: {
+              maxWidth: 32,
+              maxHeight: 32
+            },
+            sprite: '../icon.svg',
+            symbol: true,
+            render: {
+              css: {
+                dest: '../icon.css'
+              }
+            }
+          }
+        }
+      }))
+      .pipe(gulp.dest('dist/common/svg'))
+      .pipe($.connect.reload())
+});
+
 gulp.task('css', function () {
   var processors = [px2rem({remUnit: 75})];
   return gulp.src('./src/css/*.css')
-      .pipe(postcss(processors))
+      .pipe($.postcss(processors))
       .pipe(gulp.dest('./dist/css'))
-      .pipe(connect.reload());
+      .pipe($.connect.reload());
 });
 
 gulp.task('html', function () {
   return gulp.src('./dist/pages/*.html')
-      .pipe(connect.reload());
-});
-
-gulp.task('js', function () {
-  // return gulp.src('./src/js/*.js')
-  return gulp.src([
-    // path.input.commonjs + 'zepto.js',
-    path.input.commonjs + 'extend.js',
-    path.input.js + '*.js'
-  ])
-      .pipe(concat('index.js'))
-      // .pipe(uglify())
-      .pipe(gulp.dest(path.output.js))
-      .pipe(connect.reload());
+      .pipe($.connect.reload());
 });
 
 gulp.task('copy', function () {
   return gulp.src('./src/common/js/zepto.js')
-      .pipe(uglify())
+      .pipe($.uglify())
       .pipe(gulp.dest('./dist/common/js'))
-})
+});
 
 gulp.task('connect', function () {
-  connect.server({
+  $.connect.server({
     root: 'dist',
     port: '8000',
     livereload: true
   })
 });
 
-
 gulp.task('watch', function () {
   gulp.watch(['./dist/pages/*.html'], ['html'])
+  gulp.watch([path.input.commonSvg], ['svg'])
   gulp.watch(['./src/css/*.css'], ['css'])
   gulp.watch(['./src/js/*.js'], ['js'])
 });
 
-gulp.task('default', ['css', 'html', 'js', 'connect', 'watch']);
+gulp.task('default', ['css', 'svg', 'html', 'js', 'connect', 'watch']);
